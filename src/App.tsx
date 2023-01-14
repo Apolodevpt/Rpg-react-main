a93f90f2bfc8aae586cb22e22501cf04c78f18b3
Apolo Távora <apolotavorapt@gmail.com>
2023-01-14 10:17:15 -0300

    rpg-react-main

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import * as C from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
+
+const App = () => {
+  const char = useCharacter('Bonieky');
+  const char2 = useCharacter('Pedro');
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+      break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+      break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+      break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+      break;
+    }
+  }
+
+  return (
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Character x={char2.x} y={char2.y} side={char2.side} name={char2.name} />
+      </C.Map>
+    </C.Container>
+  );
+}
+
+export default App;
